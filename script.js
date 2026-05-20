c3969337dadcda02f1e4f8431e221cd73110c65c
Prajjwal Pandey <prajjwalpandey171@gmail.com>
2026-05-21 00:11:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#newgame-Btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turn0 = true;
+
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+
+const resetGame = ()=>{
+    turnO = true;
+    enableboxes();
+    msgContainer.classList.add("hide");
+}
+
+boxes.forEach((box) =>{
+    box.addEventListener("click",() =>{
+
+        if(turn0){ //player O turn
+            box.innerText = "O";
+            box.classList.add("O");
+            turn0 = false;
+        } else {
+            //player X
+            box.innerText="X";
+            box.classList.remove("O")
+            turn0 = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+const disableboxes = ()=>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+const enableboxes = ()=>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableboxes();
+}
+
+const checkWinner = () =>{
+    for(let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val !="" && pos2Val !="" && pos3Val !=""){
+            if(pos1Val === pos2Val && pos2Val===pos3Val){
+                showWinner(pos1Val);
+            }
+        }
+    }
+};
+
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
